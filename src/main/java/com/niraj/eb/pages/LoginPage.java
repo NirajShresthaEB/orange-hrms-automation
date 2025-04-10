9d46ba56d639df68dfa2f15cead6be4a4df49a43
NirajShresthaEB <niraj.shrestha@ebpearls.com>
2025-04-10 18:33:52 +0545

    initial commit

---
+++ src/main/java/com/niraj/eb/pages/LoginPage.java
@@ -0,0 +1,19 @@
+package com.niraj.eb.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends TestBase{
+	
+	//Page factory - Locators
+	
+	@FindBy (id="")
+	WebElement username;
+	
+	@FindBy (id="")
+	WebElement password;
+	
+	@FindBy (id="")
+	WebElement loginBtn;
+
+}
---
+++ src/test/java/com/niraj/eb/testcases/LoginTest.java
@@ -0,0 +1,14 @@
+package com.niraj.eb.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	
+	public void testone() {
+		
+		System.out.println("Hello");
+	}
+
+}
