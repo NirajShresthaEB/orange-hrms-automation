6633ba785a153f3ce8cf0f7f70b02bcafe813432
NirajShresthaEB <niraj.shrestha@ebpearls.com>
2025-04-11 16:10:18 +0545

    automated login feature

--- src/main/java/com/niraj/eb/pages/LoginPage.java
+++ src/main/java/com/niraj/eb/pages/LoginPage.java
@@ -2,18 +2,50 @@ package com.niraj.eb.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.niraj.eb.base.TestBase;
 
 public class LoginPage extends TestBase{
 	
 	//Page factory - Locators
 	
-	@FindBy (id="")
+	@FindBy (name="username")
 	WebElement username;
 	
-	@FindBy (id="")
+	@FindBy (name="password")
 	WebElement password;
 	
-	@FindBy (id="")
-	WebElement loginBtn;
+	@FindBy (xpath = "//button[text()=' Login ']")
+	WebElement login;
+	
+
+	
+//	@FindBy (xpath="div[@class='']")
+//	WebElement loginBtn;
+	
+	
+
+	
+	//Initializing page objects
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions
+	
+	public HomePage login(String un, String pwd) throws InterruptedException {
+
+		
+		Thread.sleep(3000);
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		login.click();
+		
+		return new HomePage();
+		
+		
+	}
 
 }
--- src/test/java/com/niraj/eb/testcases/LoginTest.java
+++ src/test/java/com/niraj/eb/testcases/LoginTest.java
@@ -1,14 +1,34 @@
 package com.niraj.eb.testcases;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+import com.niraj.eb.base.TestBase;
+import com.niraj.eb.pages.HomePage;
+import com.niraj.eb.pages.LoginPage;
+
+public class LoginTest extends TestBase{
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		loginPage = new LoginPage();
+	}
 	
 	@Test
 	
-	public void testone() {
+	public void testone() throws InterruptedException {
 		
-		System.out.println("Hello");
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		
+//		System.out.println("Hello");
 	}
 
 }
